9c15ae6aba4b0926f6887b21dc913ff1df4b9877
Lawrence McBhee <lawrence.olaiya@andela.com>
2016-03-24 19:07:22 +0100

    Add gulpfile to manage and optimize tasks

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass'),
+  paths = {
+    scripts: [
+      './assets/js/themes-list.js',
+      './assets/js/editor.js',
+      './assets/js/main.js'
+    ],
+    styles: './assets/sass/**/*.scss'
+  };
+
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'server.js',
+    verbose: true,
+    env: {
+      'NODE_ENV': 'development',
+      'DEBUG': '* -express:* -send -session -mquery -express-session ' +
+        '-engine* -socket.io:* -socket.io-parser -morgan -connect* -stylus*'
+    },
+    watch: './server',
+    ext: 'js hbs css json',
+    nodeArgs: ['--debug']
+  }).on('restart', () => {
+    console.log('Restarted!');
+  });
+});
+
+gulp.task('scripts', () => {
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('sass', () => {
+  return gulp.src(paths.styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.styles, ['sass']);
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('build', ['scripts', 'sass']);
+gulp.task('default', ['build', 'watch', 'nodemon']);
